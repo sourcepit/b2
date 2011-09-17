253a75f67a7b8d6267ccfb8a873082b10deba223
Bernd Vogt <bernd.vogt@sourcepit.org>
2011-09-17 17:15:26 +0000

    trying to run its in linux

---
+++ b2-examples/src/main/examples/reactor-build/build.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+if [ "$2" = "reactor" ] ; then
+
+echo
+echo ------------------------------------------------------------------------
+echo Starting reactor build for \'$1\'
+echo ------------------------------------------------------------------------
+echo Directory    : $PWD
+echo Command Line : $MAVEN_EXEC $MAVEN_ARGS -P buildProducts
+echo
+exec "$MAVEN_EXEC" $MAVEN_ARGS -P buildProducts
+
+elif [ "$2" = "single" ] ; then
+
+cd rcp-help
+echo
+echo ------------------------------------------------------------------------
+echo Starting single module build for \'rcp-help\'
+echo ------------------------------------------------------------------------
+echo Directory    : $PWD
+echo Command Line : $MAVEN_EXEC $MAVEN_ARGS -P deploySites
+echo
+exec "$MAVEN_EXEC" $MAVEN_ARGS -P deploySites
+
+cd ../rcp-ui
+echo
+echo ------------------------------------------------------------------------
+echo Starting single module build for \'rcp-ui\'
+echo ------------------------------------------------------------------------
+echo Directory    : $PWD
+echo Command Line : $MAVEN_EXEC $MAVEN_ARGS -P deploySites
+echo
+exec "$MAVEN_EXEC" $MAVEN_ARGS -P deploySites
+
+cd ../rcp
+echo
+echo ------------------------------------------------------------------------
+echo Starting single module build for \'rcp\'
+echo ------------------------------------------------------------------------
+echo Directory    : $PWD
+echo Command Line : $MAVEN_EXEC $MAVEN_ARGS -P useSites,buildProducts
+echo
+exec "$MAVEN_EXEC" $MAVEN_ARGS -P useSites,buildProducts
+
+else
+
+echo
+echo "You can choose either if you want to build the modules in this example all together in a reactor build or each on its own, sharing dependencies through generated update sites."
+echo
+echo "Re-run this example with \"run.bat <example> reactor\" or \"run.bat <example> single\""
+echo
+
+fi
+
