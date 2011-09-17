8d4e85c505b29ffd8342508467b1affe872cd2e1
Bernd Vogt <bernd.vogt@sourcepit.org>
2011-09-17 17:44:41 +0000

    trying to run its in linux

--- b2-integration-tests/src/test/java/org/sourcepit/beef/b2/examples/B2ExamplesIT.java
+++ b2-integration-tests/src/test/java/org/sourcepit/beef/b2/examples/B2ExamplesIT.java
@@ -20,7 +20,7 @@ public class B2ExamplesIT extends AbstractB2ExamplesIT
 
    public void testSimple_Module() throws Exception
    {
-      // execute();
+      execute();
    }
 
    public void testReactor_Build() throws Exception
@@ -32,35 +32,34 @@ public class B2ExamplesIT extends AbstractB2ExamplesIT
 
    public void testStructured_Module() throws Exception
    {
-      // execute();
+      execute();
    }
 
    public void testComposite_Module() throws Exception
    {
-      // execute();
+      execute();
    }
 
    public void testRcp_Example() throws Exception
    {
-      // execute();
+      execute();
    }
 
    public void testModule_With_Custom_Ant_Targets() throws Exception
    {
-      // execute();
-      //
-      // PropertiesMap testProperties = new LinkedPropertiesMap();
-      // testProperties.load(new File(exampleModuleDir, "target/test.properties"));
-      // assertEquals("generate-resources", testProperties.get("maven.phase"));
-      //
-      // testProperties = new LinkedPropertiesMap();
-      // testProperties.load(new File(exampleModuleDir,
-      // "org.sourcepit.b2.examples.simple.module/target/test.properties"));
-      // assertEquals("generate-resources", testProperties.get("maven.phase"));
-      //
-      // testProperties = new LinkedPropertiesMap();
-      // testProperties.load(new File(exampleModuleDir,
-      // "org.sourcepit.b2.examples.simple.module.tests/target/test.properties"));
-      // assertEquals("generate-test-resources", testProperties.get("maven.phase"));
+      execute();
+
+      PropertiesMap testProperties = new LinkedPropertiesMap();
+      testProperties.load(new File(exampleModuleDir, "target/test.properties"));
+      assertEquals("generate-resources", testProperties.get("maven.phase"));
+
+      testProperties = new LinkedPropertiesMap();
+      testProperties.load(new File(exampleModuleDir, "org.sourcepit.b2.examples.simple.module/target/test.properties"));
+      assertEquals("generate-resources", testProperties.get("maven.phase"));
+
+      testProperties = new LinkedPropertiesMap();
+      testProperties.load(new File(exampleModuleDir,
+         "org.sourcepit.b2.examples.simple.module.tests/target/test.properties"));
+      assertEquals("generate-test-resources", testProperties.get("maven.phase"));
    }
 }
