775bd06d0d35883f8348cdcbb3f71ff726302e5f
Bernd Vogt <bernd.vogt@sourcepit.org>
2011-09-23 22:04:03 +0000

    Task #24: enabled all its

--- b2-integration-tests/src/test/java/org/sourcepit/beef/b2/examples/B2ExamplesIT.java
+++ b2-integration-tests/src/test/java/org/sourcepit/beef/b2/examples/B2ExamplesIT.java
@@ -4,6 +4,10 @@
 
 package org.sourcepit.beef.b2.examples;
 
+import java.io.File;
+
+import org.sourcepit.beef.b2.common.internal.utils.LinkedPropertiesMap;
+import org.sourcepit.beef.b2.common.internal.utils.PropertiesMap;
 
 
 public class B2ExamplesIT extends AbstractB2ExamplesIT
@@ -19,43 +23,43 @@ public class B2ExamplesIT extends AbstractB2ExamplesIT
       execute();
    }
 
-   // public void testReactor_Build() throws Exception
-   // {
-   // // execute();
-   // execute("reactor");
-   // execute("single");
-   // }
-   //
-   // public void testStructured_Module() throws Exception
-   // {
-   // execute();
-   // }
-   //
-   // public void testComposite_Module() throws Exception
-   // {
-   // execute();
-   // }
-   //
-   // public void testRcp_Example() throws Exception
-   // {
-   // execute();
-   // }
-   //
-   // public void testModule_With_Custom_Ant_Targets() throws Exception
-   // {
-   // execute();
-   //
-   // PropertiesMap testProperties = new LinkedPropertiesMap();
-   // testProperties.load(new File(exampleModuleDir, "target/test.properties"));
-   // assertEquals("generate-resources", testProperties.get("maven.phase"));
-   //
-   // testProperties = new LinkedPropertiesMap();
-   // testProperties.load(new File(exampleModuleDir, "org.sourcepit.b2.examples.simple.module/target/test.properties"));
-   // assertEquals("generate-resources", testProperties.get("maven.phase"));
-   //
-   // testProperties = new LinkedPropertiesMap();
-   // testProperties.load(new File(exampleModuleDir,
-   // "org.sourcepit.b2.examples.simple.module.tests/target/test.properties"));
-   // assertEquals("generate-test-resources", testProperties.get("maven.phase"));
-   // }
+   public void testReactor_Build() throws Exception
+   {
+      // execute();
+      execute("reactor");
+      execute("single");
+   }
+
+   public void testStructured_Module() throws Exception
+   {
+      execute();
+   }
+
+   public void testComposite_Module() throws Exception
+   {
+      execute();
+   }
+
+   public void testRcp_Example() throws Exception
+   {
+      execute();
+   }
+
+   public void testModule_With_Custom_Ant_Targets() throws Exception
+   {
+      execute();
+
+      PropertiesMap testProperties = new LinkedPropertiesMap();
+      testProperties.load(new File(exampleModuleDir, "target/test.properties"));
+      assertEquals("generate-resources", testProperties.get("maven.phase"));
+
+      testProperties = new LinkedPropertiesMap();
+      testProperties.load(new File(exampleModuleDir, "org.sourcepit.b2.examples.simple.module/target/test.properties"));
+      assertEquals("generate-resources", testProperties.get("maven.phase"));
+
+      testProperties = new LinkedPropertiesMap();
+      testProperties.load(new File(exampleModuleDir,
+         "org.sourcepit.b2.examples.simple.module.tests/target/test.properties"));
+      assertEquals("generate-test-resources", testProperties.get("maven.phase"));
+   }
 }
