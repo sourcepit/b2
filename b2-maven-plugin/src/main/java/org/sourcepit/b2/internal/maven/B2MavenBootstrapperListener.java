0a19b9c1716fc170953cdc6b882c30a6b0c534af
Bernd Vogt <bernd.vogt@sourcepit.org>
2011-10-09 19:08:08 +0000

    copyright header

--- b2-maven-plugin/src/main/java/org/sourcepit/b2/internal/maven/B2MavenBootstrapperListener.java
+++ b2-maven-plugin/src/main/java/org/sourcepit/b2/internal/maven/B2MavenBootstrapperListener.java
@@ -1,6 +1,10 @@
-/*
- * Copyright (C) 2011 Bosch Software Innovations GmbH. All rights reserved.
- */
+/********************************************************************************************************************
+ * Copyright (c) 2011 Sourcepit.org and others. All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution, and is
+ * available at http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Last modification by $Author$ on $Date$
+ ********************************************************************************************************************/
 
 package org.sourcepit.b2.internal.maven;
 
