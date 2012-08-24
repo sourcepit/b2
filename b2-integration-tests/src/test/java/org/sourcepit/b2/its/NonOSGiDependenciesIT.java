a3053a5c6132874007c5d57f60a3a4a5ce452c5c
Bernd Vogt <bernd.vogt@sourcepit.org>
2012-08-24 15:32:40 +0200

    Finalized NonOSGiDependenciesIT

--- b2-integration-tests/src/test/java/org/sourcepit/b2/its/NonOSGiDependenciesIT.java
+++ b2-integration-tests/src/test/java/org/sourcepit/b2/its/NonOSGiDependenciesIT.java
@@ -7,10 +7,17 @@
 package org.sourcepit.b2.its;
 
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
 
 import java.io.File;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.Repository;
 import org.junit.Test;
 
 public class NonOSGiDependenciesIT extends AbstractB2IT
@@ -18,7 +25,7 @@ public class NonOSGiDependenciesIT extends AbstractB2IT
    @Override
    protected boolean isDebug()
    {
-      return true;
+      return false;
    }
 
    @Test
@@ -27,5 +34,32 @@ public class NonOSGiDependenciesIT extends AbstractB2IT
       final File moduleDir = getResource(getClass().getSimpleName());
       int err = build(moduleDir, "-e", "-B", "clean", "package", "-P", "!p2-repo");
       assertThat(err, is(0));
+
+      final File pomDepsSiteDir = new File(moduleDir, ".b2/pom-dependencies");
+      assertTrue(pomDepsSiteDir.exists());
+
+      final File[] bundles = new File(pomDepsSiteDir, "plugins").listFiles();
+      assertEquals(2, bundles.length);
+      assertEquals("javax.activation_1.1.0.jar", bundles[0].getName());
+      assertEquals("javax.mail.mail_1.4.2.jar", bundles[1].getName());
+
+      final Model pom = loadMavenModel(moduleDir);
+      final List<Repository> repositories = pom.getRepositories();
+      assertEquals(1, repositories.size());
+      
+      assertEquals(0, pom.getDependencies().size());
+
+      final Repository repository = repositories.get(0);
+      assertEquals("p2", repository.getLayout());
+      assertEquals(pomDepsSiteDir.toURL(), new URL(repository.getUrl()));
+
+      File[] siteDirs = new File(moduleDir, ".b2/sites").listFiles();
+      assertEquals(3, siteDirs.length);
+      for (File siteDir : siteDirs)
+      {
+         List<String> bundleNames = Arrays.asList(new File(siteDir, "target/repository/plugins").list());
+         assertTrue(bundleNames.contains("javax.activation_1.1.0.jar"));
+         assertTrue(bundleNames.contains("javax.mail.mail_1.4.2.jar"));
+      }
    }
 }
