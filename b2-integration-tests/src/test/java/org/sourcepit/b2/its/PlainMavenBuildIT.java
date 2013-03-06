cfce9ccafe27d53bd26d8ee9e5969df95a93d7bf
Bernd Vogt <bernd.vogt@sourcepit.org>
2013-03-06 14:16:35 +0100

    Bug #103: Skip b2 when no module projects where found

---
+++ b2-integration-tests/src/test/java/org/sourcepit/b2/its/PlainMavenBuildIT.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2013 Sourcepit.org contributors and others. All rights reserved. This program and the accompanying
+ * materials are made available under the terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.sourcepit.b2.its;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.io.File;
+
+import org.junit.Test;
+
+public class PlainMavenBuildIT extends AbstractB2IT
+{
+   @Override
+   protected boolean isDebug()
+   {
+      return false;
+   }
+
+   @Test
+   public void test() throws Exception
+   {
+      final File moduleDir = getResource(getClass().getSimpleName());
+      int err = build(moduleDir, "-e", "-B", "clean", "verify");
+      assertThat(err, is(0));
+   }
+
+}
